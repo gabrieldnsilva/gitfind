213b2a9976e7c20f59f5c8fb8176cd85511a1307
gabrieldnsilva <gabrieldnsilva@gmail.com>
2024-09-06 02:05:26 -0300

    Aplicacao disponivel - fetch da API Github para consulta de informacoes de usuarios e seus respectivos repositorios disponiveis para visualizacao

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+/* eslint-disable react/react-in-jsx-scope */
+import './styles.css';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>GitFind</h1>
+    </header>
+  )
+}
+
+export { Header };
